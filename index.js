61e772185f1345e45d0bc19906ae53180af7ed3e
Carlos Lantigua <clantigua954@gmail.com>
2018-11-17 16:14:57 -0500

    added react to project

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ express = require('express');
 const server = express();
 const knex = require('knex');
 const morgan = require('morgan');
+const cors = require('cors');
 const knexConfig = require('./knexfile');
 const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(morgan('short'));
+server.use(cors());
 
-server.listen(9000, () => console.log('This port is OVER 9000!!!'));
+const port = 9000;
+
+server.listen(port, (err) => {
+	err ? console.log(err) : console.log(`This Port is over ${port}!!!`);
+});
